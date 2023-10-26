bb38fcef5fafd8e6ae583442ec3d28f4c1650643
Danielle <goncalvesdani54@hotmail.com>
2023-10-26 10:44:33 -0300

    Novo branch para frontend e estilizacação das páginas

--- assessment_disc_project/settings.py
+++ assessment_disc_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'disc',
     'core',
+    'user_auth'
 ]
 
 MIDDLEWARE = [
@@ -107,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
